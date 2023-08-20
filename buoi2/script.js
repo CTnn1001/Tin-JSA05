aaa69eb4181ab79470bc8a2a75e0ee7c442b960a
CTnn1001 <chitinn401@gmail.com>
2023-08-20 12:10:56 +0700

    message

---
+++ buoi2/script.js
@@ -0,0 +1,36 @@
+
+// var myDog = {
+//     "name": "Ngao",
+
+//     "legs": 4,
+
+//     "friends": ["everything!"]
+// }
+
+// myDog.name = "husky"
+
+// myDog.color = "brown"
+
+// delete (myDog.friends)
+
+// console.log(myDog)
+
+
+// let dog1 = ["alaska", 4]
+// let dog2 = ["corgi", 5]
+
+// let dogs = [dog1, dog2]
+
+// console.log(dogs)
+
+// console.log(dogs[0][0])
+
+let gioTraiCay = [
+    ['xoai', 2],
+    ['dua hau', 3]
+]
+
+for (let i = 0; i < gioTraiCay.length; i++) {
+    console.log("gio trai cay:" + gioTraiCay[i][0] + gioTraiCay[i][1])
+}
+let i = 0
---
+++ buoi3/script.js
@@ -0,0 +1,19 @@
+// let arr = ["Toan", "Ngu Van", "Tieng Anh"]
+
+// for (let i in arr) {
+//     console.log(arr[i])
+// }
+
+// let obj = {
+//     'name': "Nguyen Van Dau",
+//     'sex': "Khong xac dinh",
+//     'crime': "Vi pham hanh chinh"
+// }
+
+// for (let i in obj)
+// console.log(obj[i])
+
+let arr = ["Toan", "Ngu Van", "Tieng Anh"]
+
+for (let value of arr)
+    console.log(value)
